e9603e668d215fa92353b4ccfa157dca6e86e1bf
khalid <dev.khalidhossain@gmail.com>
2023-09-20 22:01:29 +0600

    uuid is been set for store's home billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,20 +8,18 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("1e8d656b-bd7a-49ad-880c-185b085976f0");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard 
-          data={billboard}
-        />
+        <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
       </div>
     </Container>
-  )
+  );
 };
 
 export default HomePage;
